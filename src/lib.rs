37c1a2f37e6e4683e27077a3effbf5cd6291b3ba
arekkukula <arkadiusz.kukula@gmail.com>
2023-06-03 20:58:09 +0200

    Implement base plugin; try to lowpass a signal

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,108 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+use nih_plug::prelude::*;
+
+struct Effect {
+    params: Arc<EffectParams>,
+}
+
+#[derive(Params)]
+struct EffectParams {
+    #[id = "effect_gain"]
+    pub gain: FloatParam,
+}
+
+impl Default for Effect {
+    fn default() -> Self {
+        Self {
+            params: Arc::new(EffectParams {
+                gain: FloatParam::new("Gain", 0.5, FloatRange::Linear { min: 0., max: 1. }),
+            }),
+        }
+    }
+}
+impl Plugin for Effect {
+    const NAME: &'static str = "Test Plugin 1";
+    const VENDOR: &'static str = "arekkukula";
+    const URL: &'static str = "...";
+    const EMAIL: &'static str = "arkadiusz.kukula@gmail.com";
+
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    // The first audio IO layout is used as the default. The other layouts may be selected either
+    // explicitly or automatically by the host or the user depending on the plugin API/backend.
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[
+        AudioIOLayout {
+            main_input_channels: NonZeroU32::new(2),
+            main_output_channels: NonZeroU32::new(2),
+
+            aux_input_ports: &[],
+            aux_output_ports: &[],
+
+            // Individual ports and the layout as a whole can be named here. By default these names
+            // are generated as needed. This layout will be called 'Stereo', while the other one is
+            // given the name 'Mono' based no the number of input and output channels.
+            names: PortNames::const_default(),
+        },
+        AudioIOLayout {
+            main_input_channels: NonZeroU32::new(1),
+            main_output_channels: NonZeroU32::new(1),
+            ..AudioIOLayout::const_default()
+        },
+    ];
+
+    const MIDI_INPUT: MidiConfig = MidiConfig::None;
+    // Setting this to `true` will tell the wrapper to split the buffer up into smaller blocks
+    // whenever there are inter-buffer parameter changes. This way no changes to the plugin are
+    // required to support sample accurate automation and the wrapper handles all of the boring
+    // stuff like making sure transport and other timing information stays consistent between the
+    // splits.
+    const SAMPLE_ACCURATE_AUTOMATION: bool = true;
+
+    // If the plugin can send or receive SysEx messages, it can define a type to wrap around those
+    // messages here. The type implements the `SysExMessage` trait, which allows conversion to and
+    // from plain byte buffers.
+    type SysExMessage = ();
+    // More advanced plugins can use this to run expensive background tasks. See the field's
+    // documentation for more information. `()` means that the plugin does not have any background
+    // tasks.
+    type BackgroundTask = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        self.params.clone()
+    }
+
+    // This plugin doesn't need any special initialization, but if you need to do anything expensive
+    // then this would be the place. State is kept around when the host reconfigures the
+    // plugin. If we do need special initialization, we could implement the `initialize()` and/or
+    // `reset()` methods
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        _context: &mut impl ProcessContext<Self>,
+    ) -> ProcessStatus {
+        for (index, channel_samples) in buffer.iter_samples().enumerate() {
+            // Smoothing is optionally built into the parameters themselves
+            //let gain = self.params.gain.smoothed.next();
+            let len = &channel_samples.len();
+            for sample in channel_samples {
+                //*sample *= gain;
+                *sample = lowpass(&index, len)
+            }
+        }
+
+        ProcessStatus::Normal
+    }
+
+    // This can be used for cleaning up special resources like socket connections whenever the
+    // plugin is deactivated. Most plugins won't need to do anything here.
+    fn deactivate(&mut self) {}
+}
+
+fn lowpass(sample_index: &usize, max: &usize) -> f32 {
+    let step = sample_index / max;
+    // f(x) = -x + 1
+    step as f32 * -1. + 1.
 }
