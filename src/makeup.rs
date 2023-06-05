3bdf610323926656f9b48e036efb94893c8e4496
arekkukula <arkadiusz.kukula@gmail.com>
2023-06-05 03:19:33 +0200

    Refactor makeup to process single sample

--- src/makeup.rs
+++ src/makeup.rs
@@ -1,7 +1,5 @@
 /// A simple gain stage. Expects passing value in dB, e.g. by using
 /// `nih_plug::util::db_to_gain`
-pub fn makeup(data: &mut [f32], db: f32) {
-    for sample in data {
-        *sample *= db;
-    }
+pub fn makeup(sample: &mut f32, db: f32) {
+    *sample *= db;
 }
